376b44b0f91314abee476f3a9a6dd2799202b16c
Mohamed Magdy <112808266+MohamedMagdy-git@users.noreply.github.com>
2023-06-22 15:14:59 +0300

    AddUser.js

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -13,6 +13,35 @@ const AddUser = ({AddUserHandeler, toggleStateFuncHideForm}) => {
     gender: "",
    });
 
+   let validation = ()=> {
+    let name = input.name;
+    let age = input.age;
+    let phone = input.phone;
+    let adress = input.adress;
+    let gender = input.gender;
+    if(name === ""){
+        alert("Name is required");
+        return false;
+    }
+    if(age === ""){
+        alert("Age is required");
+        return false;
+    }
+    if(phone === ""){
+        alert("Phone is required");
+        return false;
+    }
+    if(adress === ""){
+        alert("Adress is required");
+        return false;
+    }
+    if(gender === ""){
+        alert("Gender is required");
+        return false;
+    }
+    return true;
+}
+
    
    let inputHandler = (e) => {
     let key = e.target.name;
@@ -25,6 +54,8 @@ const AddUser = ({AddUserHandeler, toggleStateFuncHideForm}) => {
        AddUserHandeler(input)
     }
     
+
+ 
     
    
     
@@ -34,26 +65,26 @@ const AddUser = ({AddUserHandeler, toggleStateFuncHideForm}) => {
       
             <div style={{marginBottom: "15px"}}>
                     <label style={{marginRight: "12px",marginBottom: "15px"}} htmlFor="name">Name</label>
-                    <input type="text" onChange={inputHandler} name="name" id="name" required value={input.name} placeholder="Enter Name Here" className={style.inputData}/>
+                    <input type="text" onChange={inputHandler} name="name" id="name" value={input.name} required placeholder="Enter Name Here" className={style.inputData}/>
             </div>
             
             <div style={{marginBottom: "15px"}}> 
 
                     <label style={{marginRight: "24px"}} htmlFor="age">Age</label>
-                    <input type="text" name="age" id="age" required value={input.age} onChange={inputHandler} placeholder="Enter Age Here" className={style.inputData}/>
+                    <input type="text" name="age" id="age" value={input.age} onChange={inputHandler} required placeholder="Enter Age Here" className={style.inputData}/>
             </div>
              
           
             <div style={{marginBottom: "15px"}}>
                 
                 <label style={{marginRight: "10px"}} htmlFor="phone">Phone</label>
-                <input type="text" name="phone" id="phone" required value={input.phone} onChange={inputHandler} placeholder="Enter Phone Here" className={style.inputData}/>
+                <input type="text" name="phone" id="phone" value={input.phone} onChange={inputHandler} required placeholder="Enter Phone Here" className={style.inputData}/>
             </div>
 
             <div style={{marginBottom: "15px"}}>
                 
                 <label style={{marginRight: "10px"}} htmlFor="adress">Adress</label>
-                <input type="adress" name="adress" id="adress" required value={input.adress} onChange={inputHandler} placeholder="Enter adress Here" className={style.inputData}/>
+                <input type="adress" name="adress" id="adress" value={input.adress} onChange={inputHandler} required placeholder="Enter adress Here" className={style.inputData}/>
             </div>  
 
             <div className={style.gander}>
@@ -75,7 +106,7 @@ const AddUser = ({AddUserHandeler, toggleStateFuncHideForm}) => {
             </div>    
             
             
-                <button className={style.btnFormSave} onClick={toggleStateFuncHideForm}>Save</button>
+                <button className={style.btnFormSave} onClick={validation}>Save</button>
                 <button className={style.btnFormCancel} type='reset'>Cancel</button>
             
         
